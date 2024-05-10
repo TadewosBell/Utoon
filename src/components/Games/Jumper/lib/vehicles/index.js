01b3e043592b23708851a3c0024ff981e626af37
Tadewosbell <tadewoswebkreator@gmail.com>
2024-05-09 21:38:06 -0400

    removed the jumper copy attempt

--- src/components/Games/Jumper/lib/vehicles/index.js
+++
@@ -1,9 +0,0 @@
-import { Car } from "./Car"
-import { MiniTruck } from "./MiniTruck"
-import { Van } from "./Van"
-import { Truck } from "./Truck"
-import { Bus } from "./Bus"
-import { Vehicle } from "./Vehicle"
-export { Vehicle }
-
-export const vehicles = [Car, MiniTruck, Van, Truck, Bus]
